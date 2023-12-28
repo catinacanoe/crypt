035d1806982899761eec281b4b9374c64c314f23
catinacanoe <catinacanoe@proton.me>
2023-12-27 20:55:34 -0800

    small edit for convenience

--- fn/status.sh
+++ fn/status.sh
@@ -7,13 +7,18 @@ function status() {
 
     encrypt 2>&1
 
+
     cd .crypt 2>&1
-    if [[ "$(git status)" == *"Your branch is ahead"* ]] || \
-       [[ "$(git status)" == *"Untracked"* ]] || \
-       [[ "$(git status)" == *"Changes"* ]]; then
+    local gitstat="$(git status 2>&1)"
+
+    if [[ "$gitstat" == *"Your branch is ahead"* ]] || \
+       [[ "$gitstat" == *"Untracked"* ]] || \
+       [[ "$gitstat" == *"Changes to be committed"* ]] || \
+       [[ "$gitstat" == *"Changes not staged"* ]]; then
 	echo && echo "this crypt is ahead of remote" && return
     else
 	echo && echo "this crypt is up to date with remote" && return
     fi
+
     cd .. 2>&1
 }
